91c8300279ce71180834f68814a39356a98125e6
césar <cesaralonso99@gmail.com>
2024-01-15 16:16:27 -0300

    board actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,19 @@
-const OrganizationIdPage = () => {
+import { db } from "@/lib/db";
+import { Form } from "./form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
 
   return (
-    <div>
-      organization page
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>
+            board name: {board.title}    
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
