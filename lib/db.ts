8d6e42218225680a71973c176354955f5c8d0fc0
césar <cesaralonso99@gmail.com>
2024-02-27 18:40:58 -0300

    postgresql setup up

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,13 @@
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const db = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV != 'production') globalThis.prisma = db;
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
