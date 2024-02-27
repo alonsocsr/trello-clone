17fc961533b1a95448bde2313311a2fe2fe39ba2
césar <cesaralonso99@gmail.com>
2024-02-27 18:55:32 -0300

    deploy

--- lib/db.ts
+++ lib/db.ts
@@ -5,9 +5,9 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 const connectionString = `${process.env.DATABASE_URL}`
-
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
+
 export const db = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
